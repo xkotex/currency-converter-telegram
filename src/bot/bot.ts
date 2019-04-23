ccd4f674275953b79f25bc7d9abd25dcf229ef98
Ivan Isaiev <swhtlover@gmail.com>
2019-04-23 20:04:13 +0300

    ddasdas

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,10 +1,22 @@
-import Telegraf from 'telegraf';
+var request = require('request');
 
-import { getConfig } from '../config/config';
+// import Telegraf from 'telegraf';
 
-const config = getConfig('bot_config_');
-const bot = new Telegraf(config.bot_section.bot_token);
-bot.start((ctx) => ctx.reply('Welcome!'));
-(<any>bot).launch().then(() => {
-  console.log('Bot started') 
+// import { getConfig } from '../config/config';
+
+// const config = getConfig('bot_config_');
+// const bot = new Telegraf(config.bot_section.bot_token);
+// bot.start((ctx) => ctx.reply('Welcome!'));
+// (<any>bot).launch().then(() => {
+//   console.log('Bot started') 
+// });
+
+const options = {
+  method: 'GET',
+  uri: 'https://risingstack.com'
+}
+
+request('https://www.eway.in.ua/ajax/ua/kharkiv/routes', { json: true }, (err: any, res: any, body: any) => {
+if (err) { return console.log(err); }
+console.log(body);
 });
